af5f3ee04357bc2fdeaf719f8fdef3eef01827be
hasannayeeem <hasannayeeem@gmail.com>
2022-05-28 23:19:06 +0600

    finally checked and update and done

--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import me from '../../assets/images/me.jpg'
 const MyPortfolio = () => {
     return (
-        <div className='px-20 py-12'>
-            <div className="text-5xl py-12 text-center">About Me</div>
-            <div className="text-3xl font-semibold py-3">Personal Info</div>
+        <div className='px-20 py-12 pb-32'>
+            <div className="text-5xl py-12 text-accent text-center">About Me</div>
+            <div className="text-3xl text-accent font-semibold py-3">Personal Info</div>
             <div class="card rounded-none lg:card-side bg-base-100 shadow-xl">
                 <figure><img className='w-72 h-96' src={me} alt="Nayeem Hasan"/></figure>
                 <div class="card-body h-96">
@@ -19,10 +19,10 @@ const MyPortfolio = () => {
                     <div className="text-xl  text-neutral"><small>Github: <span className='font-semibold'>/hasannayeeem</span></small></div>
                 </div>
                 <div class="card-body">
-                    <div className="text-2xl font-bold">SKILL</div>
+                    <div className="text-2xl text-accent font-bold">SKILL</div>
                     <div className="text-xl  text-neutral"><i><span className='font-semibold'>ReactJS, Nodejs(express), Mongodb, Firebase(authentication), Bootstrap, <br/> tailwind,
                     DaisyUI, CSS, Html </span></i></div>
-                    <div className="text-2xl font-bold">PROJECTS</div>
+                    <div className="text-2xl text-accent font-bold">PROJECTS</div>
                     <div className="text-xl  text-neutral">Autostars: <a className='text-primary' target="_blank" href='https://autostars.netlify.app'>https://autostars.netlify.app</a></div>
                     <div className="text-xl  text-neutral">Travelu: <a className='text-primary' target="_blank" href='https://traveluu.netlify.app'>https://traveluu.netlify.app</a></div>
                     <div className="text-xl  text-neutral">GamesReview: <a className='text-primary' target="_blank" href='https://gamesreview.netlify.app'>https://gamesreview.netlify.app/</a></div>
